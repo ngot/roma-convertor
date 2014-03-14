0dd80d1f21b3523bdc3292265ca0bd2938e3bf20
Hengfei Zhuang <zhuanghengfei@gmail.com>
2014-03-15 00:08:31 +0800

    add impl

--- src/index.js
+++ src/index.js
@@ -12,50 +12,41 @@
 	}
 })('roma_number', function () {
 
-	function parse(number){
-		switch(number){
-			case 1:
-			return "I";
-			case 5: 
-			return "V";
-			case 10: 
-			return "X";
-			case 50: 
-			return "L";
-			case 100: 
-			return "C";
-			case 500: 
-			return "D";
-			case 1000: 
-			return "M";
-			default:
-			return "not found!";	
-		}
+	function parse(num) {
+		if (!num)
+			return false;
+		var digits = String(+num).split(""),
+			key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM",
+				"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC",
+				"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+			roman = "",
+			i = 3;
+		while (i--)
+			roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+		return Array(+digits.join("") + 1).join("M") + roman;
 	}
 
-	function deParse(roma){
-		switch(roma){
-			case "I": 
-			return 1;
-			case "V": 
-			return 5;
-			case "X": 
-			return 10;
-			case "L": 
-			return 50;
-			case "C": 
-			return 100;
-			case "D": 
-			return 500;
-			case "M": 
-			return 1000;
-			default:
-			return "not found!";	
+	function deParse(s) {
+		s = String(s).toUpperCase();
+		if (s.length > 15 || /[^MDCLXVI]/.test(s)) return NaN;
+		var L = s.length, sum = 0, i = 0, next, val,
+			R = {
+				M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1
+			};
+		while (i < L) {
+			val = s.charAt(i++);
+			if (!R[val]) return NaN;
+			val = R[val];
+			next = R[(s.charAt(i) || 'N')] || 0;
+			if (next > val) val *= -1;
+			sum += val;
 		}
-	}	
+		if (parse(sum) == s) return sum;
+		return NaN;
+	}
 
 	return {
-		parse : parse,
+		parse: parse,
 		deParse: deParse
 	};
 });
